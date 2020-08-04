87a3cfab68b2cf800473c9e5819833bc35f7d7bb
BlertaPanduri <blertabajri@gmail.com>
2020-08-04 15:57:26 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import List from "./components/List/List.js";
+import Modal from "./components/Modal/Modal.js";
+import Backdrop from "./components/Backdrop/Backdrop.js";
 
-function App() {
+
+class App extends Component {
+  state={
+    modalIsOpen:false
+  }
+  showModal = () => {
+    this.setState({modalIsOpen:true})
+  }
+  closeModal = () => {
+    this.setState({modalIsOpen:false})
+  }
+  render(){
+
   return (
     <div className="App">
+      <h1>React Animation</h1>
+      <Modal show = {this.state.modalIsOpen} closed ={this.closeModal}/>
+      <Backdrop show = {this.state.modalIsOpen}/>
+      <button className="Button" onClick={this.showModal}> Open Modal </button>
+        <h3> Animating List</h3>
+        <List/>
 
     </div>
   );
+  }
+
 }
 
 export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import  './Modal.module.css';
+import classes from './Modal.module.css';
 
 
 const Modal = props =>{
-    const cssClasses =["Modal",props.show ? "ModalOpen":"ModalClosed"]
+   // const cssClasses =["Modal",props.show ? "ModalOpen":"ModalClosed"]
     return(
-    <div className={cssClasses.join(" ")}>
+
+    <div className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+
         <h1> Modal </h1>
         <button onClick={props.closed}>
             Cancel
